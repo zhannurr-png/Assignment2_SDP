52e9731de239041dbabc45fc55d44cad99b5b843
zhannurr-png <253750@astanait.edu.kz>
2026-09-20 20:31:50 +0500

    implement platform GUI factories

---
+++ src/ui/WindowsFactory.java
@@ -0,0 +1,12 @@
+package ui;
+
+public class WindowsFactory implements GUIFactory {
+    @Override
+    public Button createButton(){
+        return new WindowsButton();
+    }
+    @Override
+    public Checkbox createCheckbox(){
+        return new WindowsCheckbox();
+    }
+}
